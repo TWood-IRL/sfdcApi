34cc5cb08177ea27953ea4c3388d57b3023ffaa6
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2017-12-19 23:50:08 +0000

    Updating example and adding in arrayUtils through apache commons lang3

--- src/main/java/com/sf/api/examples/TestClass.java
+++ src/main/java/com/sf/api/examples/TestClass.java
@@ -1,5 +1,6 @@
 package com.sf.api.examples;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,17 +16,21 @@ public class TestClass {
 	private static SaveResult[] result ; 
 	@Before
 	public  void setUp() {
-		SObject account = new SObject("Account") ;
-		account.setSObjectField("Name", "Test Account ");
-		account.setSObjectField("RecordTypeId", RecordTypes.getId("Account", "Broker"));
-		result = PartnerSession.insert(new SObject[] {account}) ; 
+		for(int i = 0 ; i < 10 ; i ++ ){
+			SObject account = new SObject("Account") ;
+			account.setSObjectField("Name", "Test Account " + i );
+			account.setSObjectField("RecordTypeId", RecordTypes.getId("Account", "Broker"));
+			result = ArrayUtils.addAll( PartnerSession.insert(new SObject[] {account}), result)  ; 
+		}
 		
 	}
 	
 	@Test
 	public  void doingTestingStuff(){
+		for(int i = 0 ; i < 10 ; i ++ ){
+			System.out.println(result[i].getId()); 
+		}
 		
-		System.out.println(result[0].getId());
 	}
 	@After
 	public void tearDown(){ //removing the data after the test has finished this can be implemented differently if needed 
